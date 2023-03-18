842bc684d026533c951439a64851e69ef466b014
Try Catch <tcatch@dvlmnt.onmicrosoft.com>
2023-03-18 17:29:56 -0500

    added content an app layout

--- tacklebox/src/App.tsx
+++ tacklebox/src/App.tsx
@@ -1,10 +1,20 @@
 import { FC } from 'react';
 import useSPPnP from './utils/useSPPnP';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Content from './Content/Content';
 
 const App: FC = () => {
     const { userProfile } = useSPPnP();
 
-    return <>{`Welcome ${userProfile?.Title}`}</>;
+    return (
+        <>
+            <CssBaseline />
+            <Container maxWidth='sm'>
+                <Content />
+            </Container>
+        </>
+    );
 };
 
 export default App;
